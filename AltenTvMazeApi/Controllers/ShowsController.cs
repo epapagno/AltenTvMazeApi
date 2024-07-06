ec8ffc97a689184084f482dd56da4d72de100b80
Eduardo Papagno <nosoyelchuni@hotmail.com>
2024-07-06 12:25:22 +0200

    Service

---
+++ AltenTvMazeApi/Controllers/ShowsController.cs
@@ -0,0 +1,37 @@
+﻿using AltenTvMazeApi.Models;
+using AltenTvMazeApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AltenTvMazeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShowsController : ControllerBase
+    {
+        private readonly TvMazeService _tvMazeService;
+
+        public ShowsController(TvMazeService tvMazeService)
+        {
+            _tvMazeService = tvMazeService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetShow(int id)
+        {
+            var show = await _tvMazeService.GetShowByIdAsync(id);
+            if (show == null)
+            {
+                return NotFound();
+            }
+            return Ok(show);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Show>> GetAllShows()
+        {
+            return await _tvMazeService.GetAllShowsAsync();
+        }
+    }
+}
--- AltenTvMazeApi/Startup.cs
+++ AltenTvMazeApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AltenTvMazeApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace AltenTvMazeApi
 {
@@ -22,18 +24,34 @@ namespace AltenTvMazeApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHttpClient<TvMazeService>();
+            services.AddMvc().AddNewtonsoftJson();
+
+            // Configurar Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "TvMaze API",
+                    Version = "v1"
+                });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TvMaze API V1");
+                    c.RoutePrefix = string.Empty; // Para que Swagger UI esté en la raíz
+                });
             }
 
             app.UseHttpsRedirection();
